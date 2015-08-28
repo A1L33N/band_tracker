e9f463dcd18334133eda1672bb68b4b0fdc9aba3
Aileen Cacayorin <acacayorin@gmail.com>
2015-08-28 15:15:27 -0700

    Passes add venue spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -9,3 +9,14 @@ describe('adding a band', {:type => :feature}) do
     expect(page).to have_content('Mouse Rat')
   end
 end
+
+describe('adding a venue', {:type => :feature}) do
+  it 'allows a user to add a venue to the band tracker database' do
+    visit('/venues')
+    fill_in('name', :with => 'stubbs')
+    fill_in('city', :with => 'austin')
+    select('TX', :from => 'state')
+    click_button('Add Venue')
+    expect(page).to have_content('Stubbs')
+  end
+end
