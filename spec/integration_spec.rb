a5126efd5fd561f0f65da2a0c1642fdd9da83714
Aileen Cacayorin <acacayorin@gmail.com>
2015-08-28 16:38:01 -0700

    Passes bands page and venues page specs. Add css styling

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,5 +1,21 @@
 require('spec_helper')
 
+describe('visit the bands list page', {:type => :feature}) do
+  it 'takes the user to the band list page' do
+    visit('/')
+    click_link('Bands')
+    expect(page).to have_content('Bands You Follow')
+  end
+end
+
+describe('visit the venues list page', {:type => :feature}) do
+  it 'takes the user to the venues list page' do
+    visit('/')
+    click_link('Venues')
+    expect(page).to have_content('Band Venues')
+  end
+end
+
 describe('adding a band', {:type => :feature}) do
   it 'allows a user to add a band to the band tracker database' do
     visit('/bands')
